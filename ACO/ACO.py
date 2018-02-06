f8ac81ddefcad5669582d7749eced0cd71b489f8
Liaowag <844883698@qq.com>
2018-02-06 15:58:38 +0800

    second commit

---
+++ ACO/ACO.py
@@ -0,0 +1,164 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ACO:
+    def __init__(self, cities, num_ants=50):
+        '''
+
+        :param cities: 城市的数量，初始化为50
+        :param num_ants: 蚂蚁的数量，初始化为50
+        '''
+        self.cities = cities
+        self.num_cities = len(cities)
+
+        #初始化距离矩阵
+        self.Distance = np.empty((self.num_cities, self.num_cities))
+        for i in range(self.num_cities):
+            for j in range(self.num_cities):
+                V = (cities[i][0] - cities[j][0], cities[i][1] - cities[j][1])
+                self.Distance[i, j] = np.linalg.norm(V)
+        self.Pher = np.ones((self.num_cities, self.num_cities))
+
+        self.num_ants = num_ants
+        self.alpha = 1
+        self.beta = 2
+        self.rho = 0.5
+        self.Q = 100
+
+        self.bestPath_Length = float('inf')
+        self.bestPath = None
+
+    def _Ant(self):
+        '''
+        蚂蚁不断根据当前的城市节点选择下一个节点，直到走遍所有的节点，返回最终的路径
+        :return:
+        '''
+        available_cities = list(range(self.num_cities))
+        path = []
+
+        current_city = np.random.choice(available_cities)
+        available_cities.remove(current_city)
+        path.append(current_city)
+
+        for i in range(self.num_cities - 1):
+            next_city = self._NextCity(current_city, available_cities)
+            available_cities.remove(next_city)
+            path.append(next_city)
+            current_city = next_city
+
+        return path
+
+
+    def _UpdatePher(self, path_list):
+        '''
+        :param path_list: 一次迭代的路径集合
+        :return:
+        '''
+        Phers = np.zeros((self.num_cities, self.num_cities))
+        for path_Length, path in path_list:
+            amount_Pher = self.Q / path_Length
+            for i in range(-1, self.num_cities - 1):
+                Phers[path[i], path[i + 1]] += amount_Pher
+                Phers[path[i + 1], path[i]] += amount_Pher
+        self.Pher = self.Pher * self.rho + Phers
+
+    def _Length(self, path):
+        '''
+
+        :param path:  输入的路径
+        :return: 返回值为这个路径对应的路径长度
+        '''
+        Length = 0.0
+        for i in range(-1, self.num_cities - 1):
+            Length += self.Distance[path[i]][path[i + 1]]
+        return Length
+
+    def _NextCity(self, current_city, available_cities):
+        '''
+
+        :param current_city: 当前蚂蚁所在的城市
+        :param available_cities: 当前蚂蚁可以到达的城市集合
+        :return: 返回蚂蚁选择的下一个城市
+        '''
+        Probs = []
+        for i in available_cities:
+            # 根据选择城市的概率公式，得到分子的信息素浓度的alpha次方与到下一个城市启发式信息的beta次方
+            Pher = pow(self.Pher[current_city][i], self.alpha)
+            Distance = pow(self.Distance[current_city][i], self.beta)
+            Probs.append(Pher / Distance)
+        sum_weight = sum(Probs)
+        Probs = list(map(lambda x: x / sum_weight, Probs))
+        #这里根据轮盘赌方法随机选择下一个城市
+        next_city = np.random.choice(available_cities, p=Probs)
+        return next_city
+
+
+
+
+    def Iter(self, max_iter=200):
+        '''
+        算法的主要过程
+        :param max_iter: 最大的迭代次数
+        :return:
+        '''
+        bestLength = float('inf')
+        bestPath = None
+        Length_list = []
+        bestIterate=0
+        for i in range(1, max_iter + 1):
+            Length, path = self.Iter_once()
+            Length_list.append(Length)
+            print(f'Iterate times: {i}, Length: {Length:.6f}, Path: {path}')
+
+            if Length < bestLength:
+                bestLength, bestPath = Length, path
+                bestIterate=i
+
+
+        bestLength = min(Length_list)
+        print(f'The best Path is:  Iterate:{bestIterate}, Length: {bestLength:6f}')
+        self._Path(bestIterate,bestLength,bestPath)
+        return bestLength,bestPath
+
+    def Iter_once(self):
+        '''
+        一次迭代之后，更新信息素浓度，找到当前最优路径与当前最优解
+        :return:  当前最优长度与对应的路径
+        '''
+        path_list = []
+        bestLength = float('inf')
+        bestPath = None
+        for i in range(self.num_ants):
+            path = self._Ant()
+            Length = self._Length(path)
+            path_list.append((Length, path))
+            if Length < bestLength:
+                bestLength, bestPath = Length, path
+        self._UpdatePher(path_list)
+        return bestLength, bestPath
+
+    def _Path(self, Itertime, Length, path):
+        for i in range(-1, self.num_cities - 1):
+            x = [self.cities[path[i]][0], self.cities[path[i + 1]][0]]
+            y = [self.cities[path[i]][1], self.cities[path[i + 1]][1]]
+            plt.plot(x, y)
+        plt.title(f' Iteration times: {Itertime},  The path Length: {Length:.6f}')
+        plt.show()
+
+
+
+def Data(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    lines = lines[0:]
+    lines = lines[:-1]
+    lines = [c.split(' ') for c in lines]
+    cities = [(float(c[1]), float(c[2])) for c in lines]
+    return cities
+
+
+if __name__ == '__main__':
+    cities = Data('ACOtsp.tsp')
+    aco = ACO(cities, num_ants=50,)
+    aco.Iter(max_iter=50)
---
+++ GA/GA.py
@@ -0,0 +1,210 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class GA:
+    def __init__(self, cities, num_genes=90, num_children=90):
+        '''
+        初始化GA算法的参数
+        :param cities: 传入城市节点
+        :param num_genes: 父代个数
+        :param num_children: 子代个数
+        '''
+
+        self.cities = cities
+        self.num_cities = len(cities)
+        self.Distance = np.empty((self.num_cities, self.num_cities))
+        for i in range(self.num_cities):
+            for j in range(self.num_cities):
+                V = (cities[i][0] - cities[j][0],
+                          cities[i][1] - cities[j][1])
+                self.Distance[i, j] = np.linalg.norm(V)
+
+        self.num_genes = num_genes
+        self.num_children = num_children
+        #每一次迭代的交换次数
+        self.alpha = 50
+        self.recombination_prob = 0.9
+
+        #默认每次都进行突变
+        self.mutation_prob = 1
+
+        # 生成了父代的基因序列集合
+        self.genes = [Gene(self.Distance, self.alpha) for i in range(self.num_genes)]
+
+        def Iter(self, max_iter):
+            '''
+            整个算法的进化过程
+            :param max_iter:  最大的迭代次数
+            :return:
+            '''
+            self.genes = [Gene(self.Distance, self.alpha)
+                          for i in range(self.num_genes)]
+            bestgene = max(self.genes, key=lambda x: x.Length)
+            bestLength, bestPath = bestgene.Length, bestgene.Path
+            Length_list = [bestLength]
+            bestItetate = 0
+
+            # 逐次迭代，并且当前最好的个体
+            for i in range(1, max_iter + 1):
+                Length, Path = self.Iter_once()
+                Length_list.append(Length)
+                print(f'Iterate: {i}, Lenigth: {Length:.4f}')
+                if Length < bestLength:
+                    bestLength, bestPath = Length, Path
+                    bestItetate = i;
+
+            print(f'Iterate times: {bestItetate}, Length: {bestLength}, Path: {bestPath}')
+            self._Path(bestItetate, bestLength, bestPath)
+            return bestLength, bestPath
+
+    def _turePath(self, Path1, Path2, s, t):
+        '''
+        对于基因重组之后的个体，需要进行调整，避免出现重复的进行
+        :param Path1: 第一个体对应的路径
+        :param Path2: 第二个个体对应的路径
+        :param s:   开始交换节点
+        :param t:   末尾交换节点
+        :return:
+        '''
+        while len(set(Path1)) < self.num_cities:
+            for i in list(range(0, s)) + list(range(t + 1, self.num_cities)):
+                if Path1.count(Path1[i]) > 1:
+                    p1 = i
+                    break
+            p2 = Path1.index(Path1[p1], s, t + 1)
+            Path1[p1] = Path2[p2]
+
+    def _Combine(self, Path1, Path2):
+        '''
+        基因组合函数
+        :param Path1:  第一个体对应的路径
+        :param Path2:  第二个个体对应的路径
+        :return:
+        '''
+        Path1 = Path1.copy()
+        Path2 = Path2.copy()
+
+        if np.random.random() < self.recombination_prob:
+            s, t = np.random.choice(range(1, self.num_cities - 1),
+                                    size=2, replace=False)
+            s, t = min(s, t), max(s, t)
+            Path1[s:t + 1], Path2[s:t + 1] = Path2[s:t + 1], Path1[s:t + 1]
+            self._turePath(Path1, Path2, s, t)
+            self._turePath(Path2, Path1, s, t)
+        return Path1, Path2
+
+    def _Select(self, genes, number):
+        '''
+        轮盘赌算法选择出相应的父代基因
+        :param genes: 传入基因
+        :param number: 传入需要得到的子代个体
+        :return:
+        '''
+        weights = [x.score for x in genes]
+        sum_weight = sum(weights)
+        weights = [x / sum_weight for x in weights]
+        samples = np.random.choice(genes, number, p=weights, replace=False)
+        return list(samples)
+
+    def _Mutate(self, Path):
+        '''
+        突变函数
+        :param Path:需要突变的基因个体
+        :return:
+        '''
+        if np.random.random() < self.mutation_prob:
+            p1, p2 = np.random.choice(range(self.num_cities), 2, replace=False)
+            Path[p1], Path[p2] = Path[p2], Path[p1]
+
+
+    def Iter_once(self):
+        '''
+        进化一次的过程
+        :return:
+        '''
+        children = []
+        for i in range(self.num_children // 2):
+            parents = self._Select(self.genes, 10)
+            parents.sort(key=lambda x: x.Length)
+            #交叉组合
+            Path1, Path2 = self._Combine(parents[0].Path, parents[1].Path)
+            self._Mutate(Path1)
+            self._Mutate(Path2)
+            children.append(Gene(self.Distance, self.alpha, Path1))
+            children.append(Gene(self.Distance, self.alpha, Path2))
+        self.genes += children
+        self.genes = self._Select(self.genes, self.num_genes)
+        # 得到此次迭代的最好的基因
+        bestgene = max(self.genes, key=lambda x: x.Length)
+        return bestgene.Length, bestgene.Path
+
+
+    def _Path(self, Itertime, Length, Path):
+        for i in range(-1, self.num_cities - 1):
+            x = [self.cities[Path[i]][0], self.cities[Path[i + 1]][0]]
+            y = [self.cities[Path[i]][1], self.cities[Path[i + 1]][1]]
+            plt.plot(x, y)
+        plt.title(f'Current Iterate times: {Itertime}, Length: {Length:.6f}')
+        plt.show()
+
+
+
+
+def Data(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    lines = lines[0:]
+    lines = lines[:-1]
+    lines = [c.split(' ') for c in lines]
+    cities = [(float(c[1]), float(c[2])) for c in lines]
+    return cities
+
+class Gene:
+    def __init__(self, Distance, alpha, Path=None):
+        '''
+        初始化一个基因的参数，生成一个基因的初始路径，即其代表的解
+        :param Distance:
+        :param alpha: 一次交换次数
+        :param Path:
+        '''
+        self.Distance = Distance
+        self.num_cities = len(Distance)
+        self.alpha = alpha
+        # 随机生成初始的路径
+        if Path is None:
+            self.Path = list(range(self.num_cities))
+            np.random.shuffle(self.Path)
+        else:
+            self.Path = Path.copy()
+
+        self.Length = self._Length(self.Path)
+        self.score = self._Score(self.Length)
+
+
+
+    def _Score(self, Length):
+        '''
+        评估函数，根据路径的总长度进行评估
+        :param Length:
+        :return:
+        '''
+        return 1 / np.power(Length, self.alpha)
+
+
+    def _Length(self, Path):
+        '''
+        计算路径的总长度
+        :param Path: 当前的路径
+        :return: 长度
+        '''
+        Length = 0
+        for i in range(self.num_cities - 1):
+            Length += self.Distance[Path[i], Path[i + 1]]
+        Length += self.Distance[Path[-1], Path[0]]
+        return Length
+
+if __name__ == '__main__':
+    cities = Data('GAtsp.tsp')
+    ga = GA(cities, num_genes=110, num_children=110)
+    ga.Iter(max_iter=500)
